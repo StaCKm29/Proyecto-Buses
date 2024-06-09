2f1d591ffe49e41e0b2b6b7e9ec6a0112ec7af83
StaCKm29 <mamartinez2023@udec.cl>
2024-06-08 20:45:34 -0400

    Creando las opciones de buses, dependiendo del recorrido

--- src/main/java/modelos/CrearRecorrido.java
+++ src/main/java/modelos/CrearRecorrido.java
@@ -1,6 +1,18 @@
 package modelos;
 
+import java.util.ArrayList;
+
 public class CrearRecorrido {
+    private ArrayList<Bus> buses;
+    private Bus bus1 = new UnPiso(TipoAsiento.COMUN);
+    private Bus bus2 = new UnPiso(TipoAsiento.SEMICAMA);
+    private Bus bus3 = new UnPiso(TipoAsiento.SALONCAMA);
+    private Bus bus4 = new DosPisos(TipoAsiento.SALONCAMA, TipoAsiento.SALONCAMA);
+    private Bus bus5 = new DosPisos(TipoAsiento.SALONCAMA, TipoAsiento.SEMICAMA);
+    private Bus bus6 = new DosPisos(TipoAsiento.SEMICAMA, TipoAsiento.SEMICAMA);
+    private Bus bus7 = new DosPisos(TipoAsiento.SEMICAMA, TipoAsiento.COMUN);
+    private Bus bus8 = new DosPisos(TipoAsiento.COMUN, TipoAsiento.COMUN);
+
     public CrearRecorrido(Localidades partida, Localidades destino) throws MismaLocalidadException {
         if(partida == destino)
             throw new MismaLocalidadException("La partida y el destino no pueden ser iguales");
@@ -10,6 +22,10 @@ public class CrearRecorrido {
                 switch (destino){
                     case CHILLAN -> {
                         // Crear recorrido
+                        Bus bus1 = new UnPiso(TipoAsiento.COMUN);
+                        buses.add(bus1);
+                        Bus bus2 = new DosPisos(TipoAsiento.SALONCAMA, TipoAsiento.SEMICAMA);
+                        buses.add(bus2);
                     }
                     case CONCEPCION -> {
                         // Crear recorrido
