40cd9dee5906c175293142df48cb76b3520c73e1
zMRGABOz <gacastillo2023@udec.cl>
2024-06-10 14:23:06 -0400

    Modificando Clases de package viaje

--- src/main/java/modelos/viajes/FrutillarChillan.java
+++ src/main/java/modelos/viajes/FrutillarChillan.java
@@ -12,8 +12,5 @@ public class FrutillarChillan implements EstrategiaRecorrido {
     public void crearRecorrido(ArrayList<Bus> buses, UnPisoFactory unPisoFactory, DosPisosFactory dosPisosFactory) {
         buses.add(unPisoFactory.crearBus(TipoAsiento.COMUN));
         buses.add(dosPisosFactory.crearBus(TipoAsiento.SALONCAMA, TipoAsiento.SEMICAMA));
-        buses.add(unPisoFactory.crearBus(TipoAsiento.SEMICAMA));
-        buses.add(dosPisosFactory.crearBus(TipoAsiento.SALONCAMA, TipoAsiento.SEMICAMA));
-        buses.add(unPisoFactory.crearBus(TipoAsiento.COMUN));
     }
 }
