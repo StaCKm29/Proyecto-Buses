09d63b8f27ad001dae327bdc214874c6a28515e7
StaCKm29 <mamartinez2023@udec.cl>
2024-06-14 23:11:27 -0400

    Primera implementacion de clase SeleccionDeBuses

--- src/main/java/vistas/SeleccionDeBus.java
+++ src/main/java/vistas/SeleccionDeBus.java
@@ -1,8 +1,44 @@
 package vistas;
 
+import modelos.*;
+
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeleccionDeBus extends JPanel {
 
+    public SeleccionDeBus(ArrayList<Bus> buses) {
+        setLayout(new FlowLayout());
+
+        JList<Bus> busesDisponibles = new JList<>(buses.toArray(new Bus[0]));
+        //JComboBox<Bus> busesDisponibles = new JComboBox<>();
+        //buses.forEach(busesDisponibles::addItem);
+
+        JButton seleccionar = new JButton("Seleccionar");
+        seleccionar.addActionListener(e -> {
+            Bus bus = (Bus) busesDisponibles.getSelectedValue();
+            JOptionPane.showMessageDialog(this, "Bus seleccionado: " + bus);
+        });
+
+        //add(busesDisponibles);
+        add(new JScrollPane(busesDisponibles));
+        add(seleccionar);
+
+    }
+
+    public static void main(String args[]) throws MismaLocalidadException, LocalidadNullException {
+        JFrame frame = new JFrame("Seleccion de Bus");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+
+        CrearRecorrido recorrido = new CrearRecorrido(Localidades.FRUTILLAR, Localidades.SANTIAGO, LocalDate.now());
+
+        SeleccionDeBus seleccionDeBus = new SeleccionDeBus(recorrido.getBuses());
+        frame.add(seleccionDeBus);
+
+        frame.setVisible(true);
+    }
 }
