539099f555631d9f921d6af7e3a22200b099fb29
StaCKm29 <mamartinez2023@udec.cl>
2024-06-19 20:19:46 -0400

    Comenzando a implementar AsientosEnUnPiso en SeleccionDeAsiento

--- src/main/java/vistas/SeleccionDeAsiento.java
+++ src/main/java/vistas/SeleccionDeAsiento.java
@@ -9,31 +9,23 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class SeleccionDeAsiento extends JPanel {
-    private ArrayList<Asiento> AsientosDelBus;
+    private ArrayList<Asiento> asientosDelBus;
+    private AsientosEnUnPiso pisoUno;
+    private AsientosEnUnPiso pisoDos;
 
-        public SeleccionDeAsiento(Bus busSeleccionado) {
-            /*
-            setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-
-            AsientosDelBus = busSeleccionado.getAsientos(1);
-            int half = (AsientosDelBus.size() + 1) / 2; // Si el número de asientos es impar, el primer arreglo tendrá un asiento más que el segundo
-
-            for (int i = 0; i < half; i++) {
-                ImageAsiento asiento = new ImageAsiento(AsientosDelBus.get(i));
-                asiento.setBounds(0,0,20,20);
-                panel1.add(asiento);
-            }
-            for (int i = half; i < AsientosDelBus.size(); i++) {
-                ImageAsiento asiento = new ImageAsiento(AsientosDelBus.get(i));
-                asiento.setBounds(0,0,20,20);
-                panel2.add(asiento);
-            }
-            add(new JLabel("Piso 1"));
-
-             */
+    public SeleccionDeAsiento(Bus bus) {
+        if(bus.getClass() == UnPiso.class) {
+            asientosDelBus = bus.getAsientos(1);
+            pisoUno = new AsientosEnUnPiso(asientosDelBus);
+            add(pisoUno);
+            pisoDos = null;
+        } else {
+            asientosDelBus = bus.getAsientos(1);
+            pisoUno = new AsientosEnUnPiso(asientosDelBus);
+            asientosDelBus = bus.getAsientos(2);
+            pisoDos = new AsientosEnUnPiso(asientosDelBus);
         }
-
-        // Resto del código...
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Seleccion de Asiento");
